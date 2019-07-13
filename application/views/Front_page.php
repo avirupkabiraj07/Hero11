24f41da6bb0de7562d79eb58ac931b829c417a7e
avirupkabiraj07 <avirupkabiraj07@gmail.com>
2019-07-13 18:45:43 +0530

    Added a new Section contact

--- application/views/Front_page.php
+++ application/views/Front_page.php
@@ -17,13 +17,13 @@
                     <img src="/hero11/static/logo1.png" class="size">
                 </div>
                 <div class="d-flex flex-row align-items-center justify-content-between d-sm-none d-none d-md-none d-lg-block ">
-                    <a href="Login" class="btn border-white text-white mr-3">LOGIN</a>
-                    <a href="" class="btn border-white text-white ">Register</a>
+                    <a href="Login" class="btn border-primary text-success mr-3">LOGIN</a>
+                    <a href="Register" class="btn border-primary text-success ">Register</a>
                 </div>
             </nav>
 
         </section>
-        <section class="bgimage w-100 h-100" id="">
+        <section class="bgimage w-100 h-100 mb-3" id="laptop">
             <div id="carouselExampleIndicators" class="margin carousel slide d-flex container" data-ride="carousel" data-interval="2000">
                 <ol class="carousel-indicators">
                     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -55,39 +55,78 @@
             </div>
 
         </section>
-      <hr>
+        <section class="bgimage w-100 h-100 mb-3" id="mobile">
+            <div id="carouselExampleIndicators" class="margin carousel slide d-flex container" data-ride="carousel" data-interval="2000">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner d-flex">
+                    <div class="carousel-item active">
+                        
+                            <img class="d-block w-100 m-4 p-4 align-items-center" src="/hero11/static/img1.png" width="50%" height="100%" alt="First slide">
+                        
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 m-4 p-4 align-items-center" src="/hero11/static/img2.png" width="50%" height="100%" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 m-4 p-4 align-items-center" src="/hero11/static/img1.png" width="50%" height="100%" alt="Third slide">
+                    </div>
+                    
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+        </section>
         <section class="bg-dark">
         <section class="container bg-light">
             <div class="d-flex flex-column justify-content-center">
-                <div class="d-flex justify-content-center align-items-center">
-                    <h1 class="text-success">HOW TO PLAY ?</h1>
+                <div class="d-flex justify-content-center align-items-center w-100 bg-secondary mt-4 rounded">
+                    <h1 class="text-white"><b>HOW TO</b></h1>
+                    <h1 class="text-success"><b>&nbspPLAY ?</b></h1>
                 </div>
-                <div class="d-flex justify-content-center align-items-center">
-                    <h1 class="text-warning">3  SIMPLE & EASY  STEPS</h1>
+                <div class="d-flex justify-content-center align-items-center w-100 bg-secondary mt-4 rounded">
+                    <h1 class="text-success"><b>3 SIMPLE &</b></h1>
+                    <h1 class="text-white"><b>&nbspEASY STEPS!</b></h1>
                 </div>
                 <div class="d-flex flex-md-wrap p-md-5  flex-row justify-content-around align-items-center">
                     <div class="d-flex flex-md-wrap flex-column justify-content-center">
                         <div class="d-flex justify-content-center align-items-center">
-                            <img src="/hero11/static/step1.png"  width="200px" height="200px">
+                            <img src="/hero11/static/step1.png" class="rounded-circle" width="200px" height="200px">
                         </div>
-                        <div>
-                            <h3>Select a match from all ongoing matches</h3>
+                        <div class="d-flex justify-content-center align-items-center">
+                            <h3 class="text-success"><b>SELECT</b></h3>
+                            <h3 class="text-info"><b>&nbspA</b></h3>
+                            <h3 class="text-warning"><b>&nbspMATCH !</b></h3>
                         </div>
                     </div>
                     <div class="d-flex flex-md-wrap flex-column justify-content-center">
                         <div class="d-flex justify-content-center align-items-center">
-                            <img src="/hero11/static/step2.png"  width="200px" height="200px">
+                            <img src="/hero11/static/step2.png" class="rounded-circle" width="200px" height="200px">
                         </div>
-                        <div>
-                            <h3>Make your own team!</h3>
+                        <div class="d-flex justify-content-center align-items-center">
+                            <h3 class="text-info"><b>CHOOSE</b></h3>
+                            <h3 class="text-warning"><b>&nbspYOUR</b></h3>
+                            <h3 class="text-success"><b>&nbspHEROES !</b></h3>
                         </div>
                     </div>
                     <div class="d-flex flex-md-wrap flex-column justify-content-center">
                         <div class="d-flex justify-content-center align-items-center">
-                            <img src="/hero11/static/step3.png"  width="200px" height="200px">
+                            <img src="/hero11/static/step3.png" class="rounded-circle" width="200px" height="200px">
                         </div>
-                        <div>
-                            <h3>Enter the tournament and compete !</h3>
+                        <div class="d-flex justify-content-center align-items-center">
+                            <h3 class="text-warning"><b>ENTER THE</b></h3>
+                            <h3 class="text-success"><b>&nbspTOURNAMENT</b></h3>
+                            <h3 class="text-info"><b>&nbsp& COMPETE !</b></h3>
                         </div>
                     </div>
                 </div>
@@ -119,26 +158,70 @@
                 <div class="bg-dark d-flex flex-column justify-content-center">
                     <div class="text-white">
                         <div class="d-flex justify-content-center py-4">
-                            <h1><b>HELP</b></h1>
+                            <h1><b>CONTACT US</b></h1>
                         </div>
-                        <div class="d-flex justify-content-center py-4">
-                            <h1><i>CONTACT US</i></h1>
-                        </div>
-                        <div class=" d-flex flex-row justify-content-between">
-                            <div class="px-4 mx-4">
-                                <p>Avirup Kabiraj</p>
+                        <div class=" d-flex flex-row justify-content-around flex-wrap m-4">
+                            <div class="px-4 mx-4 d-flex flex-column justify-content-center">
+                                <div class="m-2 d-flex justify-content-center border-top border-bottom border-success rounded">
+                                    <img src="/hero11/static/Abhilash_Dutta.jpg">
+                                </div>
+                                <div class="mx-2 d-flex justify-content-center">
+                                    <h3><b>ABHILASH DUTTA</b></h3>
+                                </div>
+                                <div class="mx-2 d-flex justify-content-center">
+                                    <h4><i>CSE 4TH YEAR,UEMK</i></h4>
+                                </div>
+                                <div class="mx-2 d-flex justify-content-center">
+                                    <a href="mailto:abhilashdutta71@gmail.com"><h5 class="text-info">Click here to Mail Abhilash Dutta</h5></a>
+                                </div>
                             </div>
-                            <div class="px-4 mx-4">
-                                <p>Avirup Kabiraj</p>
+                            <div class="px-4 mx-4 d-flex flex-column justify-content-center">
+                                <div class="m-2 d-flex justify-content-center border-top border-bottom border-success rounded">
+                                    <img src="/hero11/static/Avirup_Kabiraj.jpg">
+                                </div>
+                                <div class="mx-2 d-flex justify-content-center">
+                                    <h3><b>AVIRUP KABIRAJ</b></h3>
+                                </div>
+                                <div class="mx-2 d-flex justify-content-center">
+                                    <h4><i>CSE 4TH YEAR,UEMK</i></h4>
+                                </div>
+                                <div class="mx-2 d-flex justify-content-center">
+                                    <a href="mailto:avirupkabiraj07@gmail.com"><h5 class="text-success">Click here to Mail Avirup Kabiraj</h5></a>
+                                </div>
                             </div>
-                            <div class="px-4 mx-4">
-                                <p>Avirup Kabiraj</p>
+                            <div class="px-4 mx-4 d-flex flex-column justify-content-center">
+                                <div class="m-2 d-flex justify-content-center border-top border-bottom border-success rounded">
+                                    <img src="/hero11/static/Aritra_Mitra.jpg">
+                                </div>
+                                <div class="mx-2 d-flex justify-content-center">
+                                    <h3><b>ARITRA MITRA</b></h3>
+                                </div>
+                                <div class="mx-2 d-flex justify-content-center">
+                                    <h4><i>CSE 4TH YEAR,UEMK</i></h4>
+                                </div>
+                                <div class="mx-2 d-flex justify-content-center">
+                                    <a href="mailto:aritrakissenmitra@gmail.com"><h5 class="text-warning">Click here to Mail Aritra Mitra</h5></a>
+                                </div>
                             </div>
 
                         </div>
                 </div>
                 
             </section>
+            <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5b8ef07dafc2c34e96e837b4/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
             <script src="/hero11/static/Front_page.js"></script>
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
